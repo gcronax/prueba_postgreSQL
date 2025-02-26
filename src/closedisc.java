5b95b133997a35af83567d237e4ed0d8d5578aa1
gcronax <146924808+gcronax@users.noreply.github.com>
2025-02-26 22:34:25 +0100

    Update and rename aux.java to closedisc.java
    dfsdf

--- src/closedisc.java
+++ src/closedisc.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class aux {
+public class closedisc {
 
     private static final String URL =
             "jdbc:postgresql://89.36.214.106:5432/geo_1cfsl_3267g";
